6058f07c4e48811b719e4e72327e3b2b7fb1683f
giomella <giomella@emeal.nttdata.com>
2024-12-05 15:04:29 +0100

    [PPANTT-197] updated integration test and smoke test action

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -55,7 +55,7 @@ data "azurerm_key_vault_secret" "key_vault_cucumber_token" {
 
 data "azurerm_key_vault_secret" "key_vault_integration_test_subkey" {
   name         = "integration-test-subkey"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
 }
 
 data "azurerm_user_assigned_identity" "workload_identity_clientid" {
