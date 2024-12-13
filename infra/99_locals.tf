b62d8c16cec1eb3d599f7a815dec46fc147329f2
giomella <giomella@emeal.nttdata.com>
2024-12-13 10:33:28 +0100

    changed product id in order to use common spontaneous payment apim product

--- infra/99_locals.tf
+++ infra/99_locals.tf
@@ -4,7 +4,7 @@ locals {
   apim = {
     name       = "${local.product}-apim"
     rg         = "${local.product}-api-rg"
-    product_id = "ebollo-gps-service"
+    product_id = "gps-spontaneous-payments-services"
   }
 }
 
