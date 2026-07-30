f3039f91915cbd78fdad18790f23e71776902d67
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-07-30 18:07:02 +0200

    fix java import formatting

--- src/test/java/it/gov/pagopa/mbd/gps/service/service/MbdGpsServiceTest.java
+++ src/test/java/it/gov/pagopa/mbd/gps/service/service/MbdGpsServiceTest.java
@@ -18,7 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
