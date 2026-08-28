000b72f7e2258993ba1d7f5082c95b24fed51ce6
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-08-28 16:09:16 +0200

    update config cache deserialization and helm values

--- src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
+++ src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.mbd.gps.service.model.cache;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
@@ -22,5 +24,18 @@ public class ConfigDataV1 {
 
   @JsonProperty("creditorInstitutions")
   @Valid
+  @Builder.Default
   private Map<String, CreditorInstitution> creditorInstitutions = new HashMap<>();
+
+  @JsonAnySetter
+  public void addCreditorInstitution(String key, CreditorInstitution value) {
+    if (!"version".equals(key)) {
+      this.creditorInstitutions.put(key, value);
+    }
+  }
+
+  @JsonAnyGetter
+  public Map<String, CreditorInstitution> getCreditorInstitutions() {
+    return creditorInstitutions;
+  }
 }
