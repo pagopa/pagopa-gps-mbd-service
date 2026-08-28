57b60cd3079a947c7d48467a1e0d828cfedf915a
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-08-28 15:20:21 +0200

    update config cache deserialization

--- src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
+++ src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.mbd.gps.service.model.cache;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
@@ -7,7 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.*;
 
-/** ConfigDataV1 */
 @Getter
 @Setter
 @Builder
@@ -20,7 +21,18 @@ public class ConfigDataV1 {
   @Builder.Default
   private String version = null;
 
-  @JsonProperty("creditorInstitutions")
   @Valid
   private Map<String, CreditorInstitution> creditorInstitutions = new HashMap<>();
+
+  @JsonAnySetter
+  public void addCreditorInstitution(String key, CreditorInstitution value) {
+    if (!"version".equals(key)) {
+      this.creditorInstitutions.put(key, value);
+    }
+  }
+
+  @JsonAnyGetter
+  public Map<String, CreditorInstitution> getCreditorInstitutions() {
+    return creditorInstitutions;
+  }
 }
