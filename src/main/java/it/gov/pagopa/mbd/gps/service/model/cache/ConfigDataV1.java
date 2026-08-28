5fcb60e542a8570879ab1694211f6767f4003831
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-08-28 15:51:05 +0200

    Revert "Merge branch 'feature_PAGOPA-3720-mbd-payment-option' of https://github.com/pagopa/pagopa-gps-mbd-service into feature_PAGOPA-3720-mbd-payment-option"
    This reverts commit c44a3e28a5467e6877191e142676672390734da9, reversing
    changes made to 57b60cd3079a947c7d48467a1e0d828cfedf915a.

--- src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
+++ src/main/java/it/gov/pagopa/mbd/gps/service/model/cache/ConfigDataV1.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.mbd.gps.service.model.cache;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
@@ -9,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.*;
 
+/** ConfigDataV1 */
 @Getter
 @Setter
 @Builder
@@ -21,18 +20,7 @@ public class ConfigDataV1 {
   @Builder.Default
   private String version = null;
 
+  @JsonProperty("creditorInstitutions")
   @Valid
   private Map<String, CreditorInstitution> creditorInstitutions = new HashMap<>();
-
-  @JsonAnySetter
-  public void addCreditorInstitution(String key, CreditorInstitution value) {
-    if (!"version".equals(key)) {
-      this.creditorInstitutions.put(key, value);
-    }
-  }
-
-  @JsonAnyGetter
-  public Map<String, CreditorInstitution> getCreditorInstitutions() {
-    return creditorInstitutions;
-  }
 }
