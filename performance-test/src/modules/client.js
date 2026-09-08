8f17bb3fec095dfebbcce9e2db0e0e4f2080ddf4
Federico Ruzzier <federicoruzzier96@gmail.com>
2026-09-08 16:50:31 +0200

    fix: change body data structure PAGOPA-3720

--- performance-test/src/modules/client.js
+++ performance-test/src/modules/client.js
@@ -4,16 +4,21 @@ import encoding from 'k6/encoding';
 export function createPaymentOption(baseUrl, subkey) {
   const url = `${baseUrl}/mbd/paymentOption`;
 
-  const datiSpecificiInnerXml = `<service>` +
-      `<amount>100</amount>` +
-      `<debtorName>Mario</debtorName>` +
-      `<debtorSurname>Rossi</debtorSurname>` +
-      `<debtorEmail>mario.rossi@example.com</debtorEmail>` +
-      `<debtorFiscalCode>RSSMRA85T10H501Z</debtorFiscalCode>` +
-      `<ciFiscalCode>77777777777</ciFiscalCode>` +
-      `<debtorProvince>MI</debtorProvince>` +
-      `<documentHash>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=</documentHash>` +
-      `</service>`;
+  const datiSpecificiInnerXml =
+    `<marcaDaBollo xmlns="http://www.agenziaentrate.gov.it/2014/MarcaDaBollo" xsi:schemaLocation="http://pagopa-api.pagopa.gov.it/pa/MarcaDaBollo.xsd" xmlns:xsi="http://www.w3.org/2001/XMLAccept-instance">` +
+    `<amount>16.00</amount>` +
+    `<debtor>` +
+    `<uniqueIdentifier>` +
+    `<entityUniqueIdentifierType>F</entityUniqueIdentifierType>` +
+    `<entityUniqueIdentifierValue>RSSMRA85T10H501Z</entityUniqueIdentifierValue>` +
+    `</uniqueIdentifier>` +
+    `<fullName>Mario Rossi</fullName>` +
+    `<province>MI</province>` +
+    `<email>mario.rossi@example.com</email>` +
+    `</debtor>` +
+    `<fiscalCode>77777777777</fiscalCode>` +
+    `<documentHash>af4WYySYOCa6Xgk+ByxIvxuaPsx1JerRgipP1xeM8bI=</documentHash>` +
+    `</marcaDaBollo>`;
 
   const base64DatiSpecifici = encoding.b64encode(datiSpecificiInnerXml);
 
@@ -22,6 +27,8 @@ export function createPaymentOption(baseUrl, subkey) {
       `<idPA>77777777777</idPA>` +
       `<idBrokerPA>77777777777</idBrokerPA>` +
       `<idStation>station1</idStation>` +
+      `<idServizio>00005</idServizio>` +
+      `<idSoggettoServizio>50000</idSoggettoServizio>` +
       `<datiSpecificiServizioRequest>${base64DatiSpecifici}</datiSpecificiServizioRequest>` +
       `</paDemandPaymentNoticeRequest>`;
 
