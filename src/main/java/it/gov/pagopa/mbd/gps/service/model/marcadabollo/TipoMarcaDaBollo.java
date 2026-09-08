3b322bce0a010f8be8614c0fd47fdff3807b8801
wazzisaa <128468629+wazzisaa@users.noreply.github.com>
2026-09-08 12:51:44 +0200

    fix documentHash validation

--- src/main/java/it/gov/pagopa/mbd/gps/service/model/marcadabollo/TipoMarcaDaBollo.java
+++ src/main/java/it/gov/pagopa/mbd/gps/service/model/marcadabollo/TipoMarcaDaBollo.java
@@ -51,7 +51,7 @@ public class TipoMarcaDaBollo implements Serializable {
   @XmlElement(name = "documentHash", required = true)
   @XmlSchemaType(name = "base64Binary")
   @NotNull(message = "Document hash is required")
-  @Size(min = 44, max = 44, message = "Document hash must be a valid 32-byte SHA-256 digest")
+  @Size(min = 32, max = 32, message = "Document hash must be a valid 32-byte SHA-256 digest")
   private byte[] documentHash;
 
 
